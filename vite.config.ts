cb5ad942fc09355d098d5db069ee7259e0328784
Philippe Gerbeau <philippe.gerbeau.93@gmail.com>
2022-12-02 17:36:53 -0500

    Remove unused dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import typescript2 from 'rollup-plugin-typescript2'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // typescript2({
-    //   check: false,
-    //   include: ['src/components/*.vue'],
-    //   tsconfigOverride: {
-    //     exclude: [
-    //       "vite.config.ts"
-    //     ]
-    //   }
-    // }),
     dts(({ insertTypesEntry: true}))
   ],
   build: {
