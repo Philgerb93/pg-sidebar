04e5b8fca06da47efc0c37156de2f79ca5a4bbe9
Philippe Gerbeau <philippe.gerbeau.93@gmail.com>
2022-12-02 18:21:17 -0500

    Modify build

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import typescript2 from 'rollup-plugin-typescript2'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    typescript2({
-      check: false,
-      include: ['src/components/*.vue'],
-      tsconfigOverride: {
-        compilerOptions: {
-          sourceMap: true,
-          declaration: true,
-          declarationMap: true,
-        },
-        exclude: [
-          "vite.config.ts"
-        ]
-      }
-    })
+    // typescript2({
+    //   check: false,
+    //   include: ['src/components/*.vue'],
+    //   tsconfigOverride: {
+    //     exclude: [
+    //       "vite.config.ts"
+    //     ]
+    //   }
+    // }),
+    dts(({ insertTypesEntry: true}))
   ],
   build: {
     cssCodeSplit: false,
